3f8021e01e120b29ac5daea1468c5309d3976142
MarisCode <mari.nieminen.it@gmail.com>
2024-04-16 10:28:02 +0300

    Location and visibility of Sign up form

--- public/user-front.js
+++ public/user-front.js
@@ -2,3 +2,9 @@ function toggleSignInForm() {
     $('#loginForm').slideDown(500);
     $("#openLoginFormButton").hide(500);
 }
+
+
+function toggleSignUpForm() {
+    $('#signupForm').slideDown(500);
+    $('#loginForm').hide(500);
+}
