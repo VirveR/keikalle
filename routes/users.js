d0150cf6481dac44d9a46d2260db7d32b320dfbb
MarisCode <mari.nieminen.it@gmail.com>
2024-04-18 10:14:49 +0300

    Access to profile page only with session

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,16 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 
+// This is for the sessions. Can be cleaned out in a better place when everything works!!!!!!!
+const auth = (req, res, next) => {
+    if (!req.session.user) {
+        return res.render("index", {
+            info: "Kirjaudu sisään, niin pääset käyttämään ohjelman kaikkia toimintoja!"
+        });
+    }
+    else return next();
+}
+
 //GET user
 router.get('/user', userController.getUser);
 
@@ -10,7 +20,7 @@ router.get('/user', userController.getUser);
 router.get('/user/:alias', userController.getUserByAlias);
 
 //GET user by id --> go to profile
-router.get('/profile', userController.getUserProfile);
+router.get('/profile', auth, userController.getUserProfile);
 
 //GET boolean whether the alias is in the database. returns json({found: true/false})
 router.get('/user/get_if_alias/:alias', userController.getBooleanIfAliasInDB);
@@ -28,3 +38,4 @@ router.post('/profile', userController.updateUser);
 router.post('/delete-profile', userController.deleteUser);
 
 module.exports = router;
+
