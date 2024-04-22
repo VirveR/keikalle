7aa75d0c058663db178cfd3c57bc993b59599c4a
MutasenAntti <113162849+MutasenAntti@users.noreply.github.com>
2024-04-22 16:10:50 +0300

    updated sharp-setting in userController to not cache pictures because caching prevented deleting original images after resizing them.

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,8 @@ const bcrypt = require('bcryptjs');
 
 const fs = require('fs');
 const path = require('path');
-/*const sharp = require('sharp');*/
+const sharp = require('sharp');
+sharp.cache(false);
 
 mongoose.connect(conString)
 .then(() => {
@@ -214,13 +215,13 @@ const uploadProfilePic = async (req, res) => {
     const userId = req.body.userIdForPictureUpload;
 
     // renaming file
-    var filename = userId + path.extname(req.file.originalname);    
+    var filename = userId  + '.jpg'; //+ path.extname(req.file.originalname);    
     var target_path = './public/images/profileimages/' + filename;
     
     async function resizeImage() {
         try {
             // resize original (req.file.path) image, keep the aspect ratio and save new file
-            await sharp(req.file.path).resize(400, 400, {fit: 'inside'}).toFile(target_path);
+            await sharp(req.file.path).resize(500, 500, {fit: 'inside'}).jpeg({ quality: 90 }).toFile(target_path);
 
             // update filename to database
             UserModel.findById(userId).then((user) => {
